b68b0a66a5456a9b278ef519db7828e9e58e5f58
Angvish Kumar <angvishvish@gmail.com>
2020-10-03 21:21:14 +0530

    Updates

---
+++ precache-manifest.6abefe2ef517265eece5b9d725d679b6.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d33fb8d43324aa3590b931d701816e9",
+    "url": "/ohlc-chart/index.html"
+  },
+  {
+    "revision": "01046b6c047620df2f12",
+    "url": "/ohlc-chart/static/css/3.c26f46d2.chunk.css"
+  },
+  {
+    "revision": "1653208d0b1f06970f02",
+    "url": "/ohlc-chart/static/css/main.4d8e7372.chunk.css"
+  },
+  {
+    "revision": "01046b6c047620df2f12",
+    "url": "/ohlc-chart/static/js/3.ba5b1af6.chunk.js"
+  },
+  {
+    "revision": "a52ba4103aa5cc7205a22925be7b14c0",
+    "url": "/ohlc-chart/static/js/3.ba5b1af6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b086bc224a67f14960a5",
+    "url": "/ohlc-chart/static/js/4.abefba9d.chunk.js"
+  },
+  {
+    "revision": "0e6d926fd42a5377559e",
+    "url": "/ohlc-chart/static/js/5.3fed21eb.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/ohlc-chart/static/js/5.3fed21eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af2710d4bdb7168ee549",
+    "url": "/ohlc-chart/static/js/6.8c2db575.chunk.js"
+  },
+  {
+    "revision": "e3b93feea552d5fd932a1758b1da5f1a",
+    "url": "/ohlc-chart/static/js/6.8c2db575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1653208d0b1f06970f02",
+    "url": "/ohlc-chart/static/js/main.810d0bca.chunk.js"
+  },
+  {
+    "revision": "1096444ed13f52e7cb4f",
+    "url": "/ohlc-chart/static/js/runtime-main.372baa44.js"
+  },
+  {
+    "revision": "7c40d2fd9f4009fa922d",
+    "url": "/ohlc-chart/static/js/xlsx.12bba608.chunk.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/ohlc-chart/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/ohlc-chart/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/ohlc-chart/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/ohlc-chart/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/ohlc-chart/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/ohlc-chart/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/ohlc-chart/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/ohlc-chart/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/ohlc-chart/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/ohlc-chart/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/ohlc-chart/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/ohlc-chart/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/ohlc-chart/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/ohlc-chart/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/ohlc-chart/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/ohlc-chart/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5f9895a0679ef4f994a24e6dfb3bd8cc.js"
+  "/ohlc-chart/precache-manifest.6abefe2ef517265eece5b9d725d679b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ohlc-chart/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
